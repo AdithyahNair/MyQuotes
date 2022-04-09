053d7f3de9aa0ea34dd461b2c2170d6c9939f0d8
Adithyah Nair <adithyah.nair@gmail.com>
2022-04-09 20:36:28 +0530

    Added nonPremiumQuotes to the tableViewCell

--- MyQuotes/Controller/QuotesViewController.swift
+++ MyQuotes/Controller/QuotesViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class QuotesViewController: UITableViewController {
+    
+    let quoteType = MyQuotes()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +18,23 @@ class QuotesViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return quoteType.nonPremiumQuotes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = quoteType.nonPremiumQuotes[indexPath.row]
+        
+        cell.textLabel?.numberOfLines = 0 // prevents truncation of text
+        
+        return cell
     }
 }
